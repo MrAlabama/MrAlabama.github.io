b43c213503c33d4887e9f9a45260e8c8539fdfd9
MrAlabama <124262830+MrAlabama@users.noreply.github.com>
2023-07-01 21:54:13 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const profileLink = document.querySelector('.profile_link_icon'),
-          textProfileLink = document.querySelector('.profile_link'),
-          infoLink = document.querySelector('.info_link'),
-          textInfoLink = document.querySelector('.info_textlink');
+    const links = document.querySelectorAll('.non-release')
 
-    profileLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        alert('Еще раз сука нажмешь, то я тебе яйца нахуй отпилю');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            alert('Еще раз сука нажмешь, то я тебе яйца нахуй отпилю');
+        });
     });
 
-    textProfileLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        alert('Еще раз сука нажмешь, то я тебе яйца нахуй отпилю');
-    });
-
-    infoLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        alert('Еще раз сука нажмешь, то я тебе яйца нахуй отпилю');
-    });
-
-    textInfoLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        alert('Еще раз сука нажмешь, то я тебе яйца нахуй отпилю');
-    });
 })
